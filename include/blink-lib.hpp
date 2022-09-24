199d3a8e2338a45b7de5fa08973bc8a337cf8678
Evan Allan <evanallan1026@gmail.com>
2022-09-24 09:24:54 -0500

    Add header to include everything else

---
+++ include/blink-lib.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file blink-lib.hpp
+ * @brief Header file for blink1_lib
+ *
+ * Including this file will give access to all classes within blink1_lib
+ */
+#pragma once
+
+#include "blink-lib/Blink1Device.hpp"
+#include "blink-lib/PatternLine.hpp"
+#include "blink-lib/PatternLineN.hpp"
+#include "blink-lib/RGB.hpp"
+#include "blink-lib/RGBN.hpp"
+
